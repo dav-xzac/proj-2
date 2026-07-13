1c56168b7bf2ee4b4bea8c4cb659a69c7f056f73
dav-xzac <davide.zaccaria00@gmail.com>
2026-07-13 21:06:09 +0200

    remove creato repo on HF -from this week behind paywall

--- scripts/deploy_space.py
+++ scripts/deploy_space.py
@@ -1,14 +1,12 @@
 import os
 from huggingface_hub import HfApi
 from datetime import datetime
-HF_TOKEN = os.environ["HF_TOKEN"]
-HF_USER = os.environ["HF_USER"]
-SPACE_NAME = os.environ["SPACE_NAME"]
+HF_TOKEN = os.getenv["HF_TOKEN"]
+HF_USER = os.getenv["HF_USER"]
+SPACE_NAME = os.getenv["SPACE_NAME"]
 
 api = HfApi(token=HF_TOKEN)
 repo_id = f"{HF_USER}/{SPACE_NAME}"
-
-api.create_repo(repo_id=repo_id, repo_type="space", space_sdk="docker", exist_ok=True)
 
 api.upload_folder(
     repo_id=repo_id,
