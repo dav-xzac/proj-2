9c2a096920f107a377bd0f475c6734df3278ed6d
dav-xzac <davide.zaccaria00@gmail.com>
2026-07-18 18:16:33 +0200

    CI/CD logic to agnostic py script migration

---
+++ scripts/push_kaggle_kernel.py
@@ -0,0 +1,17 @@
+import json
+import os
+import subprocess
+import sys
+
+# python scripts/push_kaggle_kernel.py(argv[0]) folder=model(argv[1]) kernel_slug=sentiment-retraining(argv[2])
+folder, kernel_slug = sys.argv[1], sys.argv[2]
+KAGGLE_USERNAME = os.environ["KAGGLE_USERNAME"]
+
+with open(f"{folder}/kernel-metadata_template.json") as f:
+    metadata = json.load(f)
+metadata["id"] = f"{KAGGLE_USERNAME}/{kernel_slug}"
+metadata["dataset_sources"] = [f"{KAGGLE_USERNAME}/secrets"]
+with open(f"{folder}/kernel-metadata.json", "w") as f:
+    json.dump(metadata, f)
+
+subprocess.run(["kaggle", "kernels", "push", "-p", folder, "--accelerator", "NvidiaTeslaT4"], check=True)
