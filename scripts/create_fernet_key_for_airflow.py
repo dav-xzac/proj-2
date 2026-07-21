3424e1778d8b2813996c84e183eccfab07629ab4
dav-xzac <davide.zaccaria00@gmail.com>
2026-07-21 11:49:28 +0200

    scripts update for python reproducibility

--- scripts/create_fernet_key_for_airflow.py
+++ scripts/create_fernet_key_for_airflow.py
@@ -1,8 +1,8 @@
 # pip install cryptography required
-# Use this script for generating a key 
+# Use this script to generate a key 
 # to encrypt secrets values for grafana, kaggle and hf 
 # also while deploying Airflow locally, to avoid any token exposed on DB
-# Copy the value in an env key --> FERNET_KEY=<printed_key>
+# Copy the value in an env key --> AIRFLOW_FERNET_KEY=<printed_key>
 
 from cryptography.fernet import Fernet
 
